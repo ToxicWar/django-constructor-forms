b0f0b120e8fa452f0aedbb6639272105d6744ea3
Anton Larkin <toxicwar94@yandex.ru>
2014-02-22 18:58:00 +0400

    Added views tests

---
+++ constructor_forms/tests/test_views.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+from __future__ import unicode_literals
+from django.core.urlresolvers import reverse_lazy
+from django.test import TestCase
+from ..utils import get_model
+from .. import fields
+
+Form = get_model('Form')
+Field = get_model('Field')
+
+
+class FormDetailTest(TestCase):
+    def setUp(self):
+        self.form = Form.objects.create(title='Title')
+        self.field = Field.objects.create(label='Label', field_type=fields.TEXT, form=self.form)
+
+    def test_form_detail_view_200(self):
+        response = self.client.get(reverse_lazy('FormDetail', kwargs={'slug': self.form.slug}))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_form_detail_view_404(self):
+        response = self.client.get(reverse_lazy('FormDetail', kwargs={'slug': 'some-slug'}))
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_form_detail_view_302(self):
+        data = {'field': 'Test'}
+        response = self.client.post(reverse_lazy('FormDetail', kwargs={'slug': self.form.slug}), data=data)
+
+        self.assertEqual(response.status_code, 302)
+
+
+class FormSentTest(TestCase):
+    def setUp(self):
+        self.form = Form.objects.create(title='Title')
+        self.field = Field.objects.create(label='Label', field_type=fields.TEXT, form=self.form)
+
+    def test_form_detail_view_200(self):
+        response = self.client.get(reverse_lazy('FormSent', kwargs={'slug': self.form.slug}))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_form_detail_view_404(self):
+        response = self.client.get(reverse_lazy('FormSent', kwargs={'slug': 'some-slug'}))
+
+        self.assertEqual(response.status_code, 404)
